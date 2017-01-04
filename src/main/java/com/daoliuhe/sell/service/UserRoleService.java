b96d5eede8225821ec119ab5db5ec804c3de30c4
caiyingying <caiyingying234@gmail.com>
2017-01-04 21:25:37 +0800

    用户页面

---
+++ src/main/java/com/daoliuhe/sell/service/UserRoleService.java
@@ -0,0 +1,31 @@
+package com.daoliuhe.sell.service;
+
+
+import com.daoliuhe.sell.model.UserRole;
+
+public interface UserRoleService {
+	
+	/**
+	 * 获取角色的分页数据
+	 * @param relation
+	 * @return
+	 */
+	Object getRolePageData(UserRole relation);
+	
+
+	/**
+	 * 根据对象属性保存关系表数据
+	 * @param relation
+	 * @return
+	 */
+	Object saveRelation(UserRole relation);
+
+	/**
+	 * 根据用户的id和角色的多个id删除关系表中的数据
+	 * @param userId
+	 * @param roleIds
+	 * @return
+	 */
+	Object deleteForUser(String userId, String roleIds);
+	
+}
